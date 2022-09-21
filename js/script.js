9ec8deee126a716ac16cc1eba46b56d068229fe5
jong2yu <jong2yu@gmail.com>
2022-09-21 17:23:52 +0900

    to project

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  // Counter Up
+  $('#skills h6').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Slider
+  $('#project .slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: true,
+    dots: true,
+    prevArrow : "<span class='material-symbols-outlined slick-prev'>chevron_left</span>",
+    nextArrow : "<span class='material-symbols-outlined slick-next'>chevron_right</span>"
+  });
+
+});
